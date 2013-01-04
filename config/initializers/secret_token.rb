48c0618bfae28a89dfd81d96ac947b4de58a6751
Yue Li <yueli@uga.edu>
2013-01-04 17:50:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giluga::Application.config.secret_token = 'c5424ec0df41f05b1fac364e80b85f2965d418e3fd76d6a91db7bba4e08f55f55114b5e4d7ba1f2a91c2aa8520935ef2c5e53bd14cc47f2def9d26dd6b8cc162'
